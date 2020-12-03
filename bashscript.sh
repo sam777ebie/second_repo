c5ba67059f08f29e55fd446a492b885f7512b0af
sam777ebie <sam777ebie@gmail.com>
2020-12-03 11:38:29 +0530

    adding a bash script

---
+++ bashscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This is my first bash script"
+echo "The shabang is a funny name for #!"
+echo "Well..."
+Name="Ebie"
+LuckyNumber=7 
+echo "Name is " $Name
+echo " and lucky number is " $LuckyNumber
